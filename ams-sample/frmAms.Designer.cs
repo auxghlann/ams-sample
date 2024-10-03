9f59b911b7777841b903429da0bd8eb82d95bf1c
auxghlann <orenoytemail@gmail.com>
2024-10-03 12:24:07 +0800

    updated desinger

--- ams-sample/frmAms.Designer.cs
+++ ams-sample/frmAms.Designer.cs
@@ -29,10 +29,12 @@
         private void InitializeComponent()
         {
             this.grdData = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtIDnumber = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.rdrTimeIn = new System.Windows.Forms.RadioButton();
+            this.rdrTimeOut = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.grdData)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,60 +42,98 @@
             // 
             this.grdData.AllowUserToAddRows = false;
             this.grdData.AllowUserToDeleteRows = false;
+            this.grdData.AllowUserToResizeColumns = false;
+            this.grdData.AllowUserToResizeRows = false;
+            this.grdData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.grdData.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.grdData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.grdData.Location = new System.Drawing.Point(51, 37);
             this.grdData.Name = "grdData";
+            this.grdData.ReadOnly = true;
             this.grdData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.grdData.Size = new System.Drawing.Size(687, 305);
             this.grdData.TabIndex = 0;
             // 
-            // button1
+            // btnSave
             // 
-            this.button1.Location = new System.Drawing.Point(524, 376);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 40);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.Location = new System.Drawing.Point(545, 376);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(86, 40);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
-            // button2
+            // btnCancel
             // 
-            this.button2.Location = new System.Drawing.Point(652, 376);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(86, 40);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancel.Location = new System.Drawing.Point(652, 376);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(86, 40);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // txtIDnumber
             // 
-            this.textBox1.Location = new System.Drawing.Point(370, 387);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(123, 20);
-            this.textBox1.TabIndex = 3;
+            this.txtIDnumber.Location = new System.Drawing.Point(282, 395);
+            this.txtIDnumber.Name = "txtIDnumber";
+            this.txtIDnumber.Size = new System.Drawing.Size(123, 20);
+            this.txtIDnumber.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(370, 368);
+            this.label1.Location = new System.Drawing.Point(282, 376);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(56, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "ID number";
+            // 
+            // rdrTimeIn
+            // 
+            this.rdrTimeIn.AutoSize = true;
+            this.rdrTimeIn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rdrTimeIn.Location = new System.Drawing.Point(434, 376);
+            this.rdrTimeIn.Name = "rdrTimeIn";
+            this.rdrTimeIn.Size = new System.Drawing.Size(59, 17);
+            this.rdrTimeIn.TabIndex = 5;
+            this.rdrTimeIn.TabStop = true;
+            this.rdrTimeIn.Text = "Time in";
+            this.rdrTimeIn.UseVisualStyleBackColor = true;
+            // 
+            // rdrTimeOut
+            // 
+            this.rdrTimeOut.AutoSize = true;
+            this.rdrTimeOut.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rdrTimeOut.Location = new System.Drawing.Point(434, 399);
+            this.rdrTimeOut.Name = "rdrTimeOut";
+            this.rdrTimeOut.Size = new System.Drawing.Size(66, 17);
+            this.rdrTimeOut.TabIndex = 5;
+            this.rdrTimeOut.TabStop = true;
+            this.rdrTimeOut.Text = "Time out";
+            this.rdrTimeOut.UseVisualStyleBackColor = true;
             // 
             // frmAms
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rdrTimeOut);
+            this.Controls.Add(this.rdrTimeIn);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtIDnumber);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
             this.Controls.Add(this.grdData);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmAms";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Attendance Monitoring";
+            this.Load += new System.EventHandler(this.frmAms_Load);
             ((System.ComponentModel.ISupportInitialize)(this.grdData)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -103,10 +143,12 @@
         #endregion
 
         private System.Windows.Forms.DataGridView grdData;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtIDnumber;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rdrTimeIn;
+        private System.Windows.Forms.RadioButton rdrTimeOut;
     }
 }
 
--- ams-sample/frmAms.cs
+++ ams-sample/frmAms.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace ams_sample
 {
@@ -16,5 +17,59 @@ namespace ams_sample
         {
             InitializeComponent();
         }
+
+        Database _db = new Database();
+
+        private OleDbCommand command;
+        private OleDbDataAdapter adapter;
+        private DataTable dt;
+
+
+        // Helper Functions
+
+        private void fill_dataGrid()
+        {
+            _db.get_all_attendances(_db.Connection, dt = new DataTable(), grdData);
+        }
+
+        private string get_time_status()
+        {
+            string time_status = "";
+
+            if (rdrTimeIn.Checked)
+            {
+                time_status = rdrTimeIn.Text;
+            }
+            else if (rdrTimeOut.Checked) { }
+            {
+                time_status = rdrTimeOut.Text;
+            }
+
+
+            return time_status;
+        }
+
+        // WinForm functions
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            int id_number = Convert.ToInt32(txtIDnumber.Text);
+            string time_status = get_time_status();
+
+            _db.add_attendance(id_number, time_status, _db.Connection);
+
+            fill_dataGrid();
+
+        }
+
+        private void frmAms_Load(object sender, EventArgs e)
+        {
+            fill_dataGrid();
+        }
+
+
+       
+
+
     }
 }
