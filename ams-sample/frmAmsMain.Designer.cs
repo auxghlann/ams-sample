42816fc70768679dab1721a1f3a9dd1cbdae3eb5
auxghlann <orenoytemail@gmail.com>
2024-10-04 14:23:32 +0800

    renamed the former main form (frmAms) and added new functionalities to accomodate new form added

---
+++ ams-sample/frmAmsMain.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace ams_sample
+{
+    partial class frmAmsMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdData = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtIDnumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rdrTimeIn = new System.Windows.Forms.RadioButton();
+            this.rdrTimeOut = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripManageStudent = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.toolStripClearAttendance = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grdData
+            // 
+            this.grdData.AllowUserToAddRows = false;
+            this.grdData.AllowUserToDeleteRows = false;
+            this.grdData.AllowUserToResizeColumns = false;
+            this.grdData.AllowUserToResizeRows = false;
+            this.grdData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grdData.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.grdData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdData.Location = new System.Drawing.Point(56, 88);
+            this.grdData.Name = "grdData";
+            this.grdData.ReadOnly = true;
+            this.grdData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdData.Size = new System.Drawing.Size(687, 305);
+            this.grdData.TabIndex = 6;
+            this.grdData.SelectionChanged += new System.EventHandler(this.grdData_SelectionChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.White;
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnSave.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSave.Location = new System.Drawing.Point(438, 428);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(86, 40);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCancel.Location = new System.Drawing.Point(657, 429);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(86, 40);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtIDnumber
+            // 
+            this.txtIDnumber.Location = new System.Drawing.Point(204, 448);
+            this.txtIDnumber.Name = "txtIDnumber";
+            this.txtIDnumber.Size = new System.Drawing.Size(123, 20);
+            this.txtIDnumber.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(204, 429);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID number";
+            // 
+            // rdrTimeIn
+            // 
+            this.rdrTimeIn.AutoSize = true;
+            this.rdrTimeIn.Checked = true;
+            this.rdrTimeIn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rdrTimeIn.Location = new System.Drawing.Point(356, 429);
+            this.rdrTimeIn.Name = "rdrTimeIn";
+            this.rdrTimeIn.Size = new System.Drawing.Size(59, 17);
+            this.rdrTimeIn.TabIndex = 1;
+            this.rdrTimeIn.TabStop = true;
+            this.rdrTimeIn.Text = "Time in";
+            this.rdrTimeIn.UseVisualStyleBackColor = true;
+            // 
+            // rdrTimeOut
+            // 
+            this.rdrTimeOut.AutoSize = true;
+            this.rdrTimeOut.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rdrTimeOut.Location = new System.Drawing.Point(356, 452);
+            this.rdrTimeOut.Name = "rdrTimeOut";
+            this.rdrTimeOut.Size = new System.Drawing.Size(66, 17);
+            this.rdrTimeOut.TabIndex = 2;
+            this.rdrTimeOut.Text = "Time out";
+            this.rdrTimeOut.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(346, 28);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Attendance Monitoring System";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(794, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripManageStudent,
+            this.toolStripClearAttendance});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(50, 20);
+            this.toolStripMenuItem1.Text = "&Menu";
+            // 
+            // toolStripManageStudent
+            // 
+            this.toolStripManageStudent.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripManageStudent.Name = "toolStripManageStudent";
+            this.toolStripManageStudent.Size = new System.Drawing.Size(180, 22);
+            this.toolStripManageStudent.Text = "Manage Student";
+            this.toolStripManageStudent.Click += new System.EventHandler(this.toolStripManageStudent_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(548, 429);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(86, 39);
+            this.btnDel.TabIndex = 9;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // toolStripClearAttendance
+            // 
+            this.toolStripClearAttendance.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripClearAttendance.Name = "toolStripClearAttendance";
+            this.toolStripClearAttendance.Size = new System.Drawing.Size(180, 22);
+            this.toolStripClearAttendance.Text = "Clear Attendance";
+            this.toolStripClearAttendance.Click += new System.EventHandler(this.toolStripClearAttendance_Click);
+            // 
+            // frmAmsMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 505);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rdrTimeOut);
+            this.Controls.Add(this.rdrTimeIn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIDnumber);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.grdData);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmAmsMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Attendance Monitoring";
+            this.Load += new System.EventHandler(this.frmAms_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdData;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtIDnumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rdrTimeIn;
+        private System.Windows.Forms.RadioButton rdrTimeOut;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripManageStudent;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.ToolStripMenuItem toolStripClearAttendance;
+    }
+}
+
---
+++ ams-sample/frmAmsMain.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ams_sample
+{
+    public partial class frmAmsMain : Form
+    {
+        public frmAmsMain()
+        {
+            InitializeComponent();
+        }
+
+        Database _db = new Database();
+
+        private OleDbCommand command;
+        private OleDbDataAdapter adapter;
+        private DataTable dt;
+
+        private int selected_id_num = -1;
+
+
+        // Forms
+        private frmAmsStudent frmStudent;
+
+
+        // Helper Functions
+
+        private void fill_dataGrid()
+        {
+            _db.get_all_attendances(_db.Connection, dt = new DataTable(), grdData);
+        }
+
+        private string get_time_status()
+        {
+            string time_status = "";
+
+            if (rdrTimeIn.Checked)
+            {
+                time_status = rdrTimeIn.Text;
+            }
+            else if (rdrTimeOut.Checked) { }
+            {
+                time_status = rdrTimeOut.Text;
+            }
+
+
+            return time_status;
+        }
+
+        // WinForm functions
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(txtIDnumber.Text) || !(rdrTimeIn.Checked || rdrTimeOut.Checked))
+            {
+                MessageBox.Show("Id Number and Time Status should not be empty", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int id_number = Convert.ToInt32(txtIDnumber.Text);
+            string time_status = get_time_status();
+
+            _db.add_attendance(id_number, time_status, _db.Connection);
+
+            fill_dataGrid();
+
+        }
+
+        private void frmAms_Load(object sender, EventArgs e)
+        {
+            fill_dataGrid();
+            btnDel.Enabled = false;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtIDnumber.Clear();
+            rdrTimeOut.Checked = false;
+            rdrTimeIn.Checked = false;
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+
+            DialogResult dg = MessageBox.Show("Are you sure you want to delete the selected record?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (dg == DialogResult.Yes)
+            {
+                _db.remove_attendance(selected_id_num, _db.Connection);
+            }
+
+        }
+
+        private void toolStripClearAttendance_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Do you want to clear the attendance?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (res == DialogResult.Yes)
+            {
+                DialogResult res2 = MessageBox.Show("Doing this action will delete all the attendance from the database. Do you want to proceed?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+
+                if (res2 == DialogResult.Yes)
+                {
+                    _db.clear_all_attendance(_db.Connection);
+                    fill_dataGrid();
+                }
+
+            }
+        }
+
+        private void grdData_SelectionChanged(object sender, EventArgs e)
+        {
+
+            if (grdData.SelectedRows.Count > 0)
+            {
+
+                var selectedRow = grdData.SelectedRows[0].DataBoundItem as DataRowView;
+                
+                if (selectedRow != null)
+                {
+                    btnDel.Enabled = true;
+
+                    this.selected_id_num = Convert.ToInt32(selectedRow["ID_number"]);
+                }
+
+
+            }
+        }
+
+        private void toolStripManageStudent_Click(object sender, EventArgs e)
+        {
+            frmStudent = new frmAmsStudent(this);
+            frmStudent.Show();
+            this.Hide();
+            
+            
+            
+        }
+    }
+}
