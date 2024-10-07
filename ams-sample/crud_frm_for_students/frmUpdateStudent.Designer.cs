c1e688074c5877c08d718dcb31b36b5d53508ed4
auxghlann <orenoytemail@gmail.com>
2024-10-07 20:40:38 +0800

    added a form for updating students

---
+++ ams-sample/crud_frm_for_students/frmUpdateStudent.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ams_sample.crud_frm_for_students
+{
+    partial class frmUpdateStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboYear = new System.Windows.Forms.ComboBox();
+            this.cboProgram = new System.Windows.Forms.ComboBox();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.txtIdNum = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(537, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Year";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(293, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(429, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Program";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(163, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "First Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "ID Number";
+            // 
+            // cboYear
+            // 
+            this.cboYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboYear.FormattingEnabled = true;
+            this.cboYear.Location = new System.Drawing.Point(540, 62);
+            this.cboYear.Name = "cboYear";
+            this.cboYear.Size = new System.Drawing.Size(45, 21);
+            this.cboYear.TabIndex = 14;
+            // 
+            // cboProgram
+            // 
+            this.cboProgram.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboProgram.FormattingEnabled = true;
+            this.cboProgram.Location = new System.Drawing.Point(432, 62);
+            this.cboProgram.Name = "cboProgram";
+            this.cboProgram.Size = new System.Drawing.Size(78, 21);
+            this.cboProgram.TabIndex = 13;
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(296, 62);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(100, 20);
+            this.txtLName.TabIndex = 12;
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(166, 62);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(100, 20);
+            this.txtFName.TabIndex = 11;
+            // 
+            // txtIdNum
+            // 
+            this.txtIdNum.Location = new System.Drawing.Point(32, 62);
+            this.txtIdNum.Name = "txtIdNum";
+            this.txtIdNum.Size = new System.Drawing.Size(105, 20);
+            this.txtIdNum.TabIndex = 10;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCancel.Location = new System.Drawing.Point(325, 116);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(100, 38);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSave.Location = new System.Drawing.Point(185, 116);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(98, 38);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmUpdateStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 196);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboYear);
+            this.Controls.Add(this.cboProgram);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.txtIdNum);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmUpdateStudent";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Update Student";
+            this.Load += new System.EventHandler(this.frmUpdateStudent_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboYear;
+        private System.Windows.Forms.ComboBox cboProgram;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.TextBox txtIdNum;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
---
+++ ams-sample/crud_frm_for_students/frmUpdateStudent.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ams_sample.crud_frm_for_students
+{
+    public partial class frmUpdateStudent : Form
+    {
+
+        private Database _db;
+
+
+        // Selected row of gridData holder
+        private int selected_id_num, selected_year;
+        private string selected_fName, selected_lName, selected_program;
+
+        public frmUpdateStudent(int id_num, string fname, string lname, string program, int year, Database db)
+        {
+            InitializeComponent();
+            InitializeProgramCBO();
+            InitializeYearCBO();
+            this.selected_id_num = id_num;
+            this.selected_fName = fname;
+            this.selected_lName = lname;
+            this.selected_program = program;
+            this.selected_year = year;
+            this._db = db;
+        }
+
+
+        // Helper function
+
+        private void InitializeProgramCBO()
+        {
+            cboProgram.Items.Add("BSCS");
+            cboProgram.Items.Add("BSIT");
+        }
+
+
+        private void InitializeYearCBO()
+        {
+            cboYear.Items.Add("1");
+            cboYear.Items.Add("2");
+            cboYear.Items.Add("3");
+            cboYear.Items.Add("4");
+        }
+
+
+        // WinForm FUnctions
+        private void frmUpdateStudent_Load(object sender, EventArgs e)
+        {
+            this.txtIdNum.Text = selected_id_num.ToString();
+            this.txtFName.Text = selected_fName;
+            this.txtLName.Text = selected_lName;
+            this.cboProgram.Text = selected_program;
+            this.cboYear.Text = selected_year.ToString();
+
+        }
+
+
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            _db.update_student(Convert.ToInt32(txtIdNum.Text), txtFName.Text, txtLName.Text, 
+                        cboProgram.Text, Convert.ToInt16(cboYear.Text), this._db.Connection);
+            this.Close();
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+
+    }
+}
